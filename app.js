8bfe25f514e60cf22225344a97bc356df35548f9
Sam <djcsdj@hotmail.com>
2025-03-07 08:42:21 +0200

    Created TodoController.createTodo integration test and fixed unit test for using database

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const app=express()
 const todoRoutes=require("./routes/todo.routes")
+const mongodb=require("./mongodb/mongodb.connect")
+
+mongodb.connect()
+app.use(express.json())
 
 app.use("/todos", todoRoutes)
 
@@ -8,8 +12,9 @@ app.get('/', (req,res)=>{
     res.send('express test')
 })
 
+/*
 app.listen(3015,()=>{
     console.log('server is running')
 })
-
+*/
 module.exports=app
